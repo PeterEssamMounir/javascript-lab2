2d107f2908917fc7589e4e01e1e58bebeb5743fe
Peter Essam <petros.guirguis@hotmail.com>
2023-03-21 14:51:29 +0200

    second commit

--- task3.js
+++ task3.js
@@ -1,11 +1,11 @@
 function sortArray() {
-    const input = document.getElementById("numbers").value;
-    const arr = input.split(",").map(Number);
+  const input = document.getElementById("numbers").value;
+  const arr = input.split(",").map(Number);
 
-    const descendingArr = arr.sort((a, b) => b - a);
-    const ascendingArr = arr.sort((a, b) => a - b);
+  const descendingArr = arr.slice().sort((a, b) => b - a);
+  const ascendingArr = arr.slice().sort((a, b) => a - b);
 
-    const output = `You have entered values of ${arr.join(", ")}<br>and your values after being sorted descending ${descendingArr.join(", ")}<br>and your values after being sorted ascending ${ascendingArr.join(", ")}`;
+  const output = `You have entered values of ${arr.join(", ")},<br>and your values after being sorted descending ${descendingArr.join(", ")},<br>and your values after being sorted ascending ${ascendingArr.join(", ")}`;
 
-    document.getElementById("output").innerHTML = output;
-  }
+  document.getElementById("output").innerHTML = `<h2>${output}</h2>`;
+}

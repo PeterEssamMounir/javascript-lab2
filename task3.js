a790d0886a5e9dd778ba915a3bac536bce8f2a31
Peter Essam <petros.guirguis@hotmail.com>
2023-03-21 15:02:14 +0200

    final commit

--- task3.js
+++ task3.js
@@ -1,6 +1,6 @@
 function sortArray() {
   const input = document.getElementById("numbers").value;
-  const arr = input.split("").map(Number);
+  const arr = input.split(",").map(Number);
 
   const ascendingArr = arr.slice().sort((a, b) => a - b);
   const descendingArr = arr.slice().sort((a, b) => b - a);
